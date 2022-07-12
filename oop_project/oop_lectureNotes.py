6ea19a0434b2ad5924cce81475833efad479ee57
Edelweiss Dunuan <edelweiss.dunuan@ecorus.com>
2022-07-12 11:38:53 +0200

    Lecture notes: 12-07-2022

---
+++ oop_project/oop_lectureNotes.py
@@ -0,0 +1,6 @@
+# OBJECT ORIENTED PROGRAMMING
+#   Classes: attribites and methods
+#   - it is always capitalised
+#   - any class can be considered as a new data type
+#   - methods are mainly functions, but since I put it inside my class, 
+#       then technically, it becomes a method(like any other method)
--- practice/objectOrientedProg.py
+++ practice/objectOrientedProg.py
@@ -3,4 +3,4 @@
 #   - it is always capitalised
 #   - any class can be considered as a new data type
 #   - methods are mainly functions, but since I put it inside my class, 
-#       then technically, it becomes a method(like any other method)
+#       then technically, it becomes a method(like any other method) 
